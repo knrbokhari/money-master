cc2ec2dbb73fbd900e4c04e8feb0a4adbf89c4f3
naeem <kazinaeemrayhan@gmail.com>
2022-02-17 03:31:47 +0600

    saving added

--- javascript/app.js
+++ javascript/app.js
@@ -5,12 +5,26 @@ function inputChacking(inputFields, field) {
             return input.value;
         }
         else {
-            return alert("Give a prime number on " + field + ' field')
+            return alert("Give a prime number in " + field + ' field')
         }
     }
     else {
-        return alert('please give only number on ' + field + ' field')
+        return alert('please give only number in ' + field + ' field')
     }
+}
+function calculatingBalances(expensesOrSaving, incomOrBalance, totals, balances, errorAlert) {
+
+    if (expensesOrSaving <= incomOrBalance) {
+        totals.innerText = expensesOrSaving;
+        balances.innerText = incomOrBalance - expensesOrSaving;
+    }
+    else if (incomOrBalance < expensesOrSaving) {
+        alert('Your total ' + errorAlert + ' are higher than your income.')
+    }
+    else {
+        alert("Please Provide valid information.")
+    }
+
 }
 document.getElementById('calculate').addEventListener("click", function () {
     // get all input value
@@ -20,25 +34,22 @@ document.getElementById('calculate').addEventListener("click", function () {
     const clothes = inputChacking('clothes-field', 'clothes');
 
     const totalExpenses = document.getElementById('total-expenses');
-    const expenses = parseInt(food) + parseInt(rent) + parseInt(clothes);
-
-
     const balance = document.getElementById('balance');
 
+    const expenses = parseInt(food) + parseInt(rent) + parseInt(clothes);
 
-    if (expenses <= income) {
-        totalExpenses.innerText = expenses;
-        balance.innerText = parseInt(income) - expenses;
-    }
-    else if (income < expenses) {
-        alert('your total expenses is higher then income')
-    }
-    else {
-        alert("give valid data")
-    }
-
+    calculatingBalances(expenses, income, totalExpenses, balance, 'expenses');
 })
 
 document.getElementById('saving').addEventListener('click', function () {
     const savingPercent = inputChacking('percent-input', 'save');
+    const totalSaving = document.getElementById('saving-amount');
+    const remainingBalance = document.getElementById('remaining-balance');
+    const balance = document.getElementById('balance');
+
+    const saving = balance.innerText / 100 * savingPercent;
+
+    if (0 < saving) {
+        calculatingBalances(saving, balance.innerText, totalSaving, remainingBalance, 'savings');
+    }
 })
