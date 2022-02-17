9693481cffae90f08b3dc352278d98dfd0ca18a3
naeem <kazinaeemrayhan@gmail.com>
2022-02-17 14:57:00 +0600

    complete all

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,4 @@
+// input chacking
 function inputChacking(inputFields, field) {
     const input = document.getElementById(inputFields);
     if (isNaN(input.value) == false) {
@@ -5,51 +6,76 @@ function inputChacking(inputFields, field) {
             return input.value;
         }
         else {
-            return alert("Give a prime number in " + field + ' field')
+            alert("Give a prime number in " + field + ' field');
+            input.value = '';
         }
     }
     else {
-        return alert('please give only number in ' + field + ' field')
+        alert('please give only number in ' + field + ' field');
+        input.value = '';
     }
 }
-function calculatingBalances(expensesOrSaving, incomOrBalance, totals, balances, errorAlert) {
 
-    if (expensesOrSaving <= incomOrBalance) {
+// calculating balances
+function calculatingBalances(expensesOrSaving, totals, balances, errorAlert, isRemainingBalance) {
+    const income = inputChacking('income-field', 'income');
+    const balance = document.getElementById('balance');
+
+
+    if (expensesOrSaving <= income) {
         totals.innerText = expensesOrSaving;
-        balances.innerText = incomOrBalance - expensesOrSaving;
-    }
-    else if (incomOrBalance < expensesOrSaving) {
-        alert('Your total ' + errorAlert + ' are higher than your income.')
+        if (isRemainingBalance == false) {
+            balances.innerText = income - expensesOrSaving;
+        }
+        else {
+            balances.innerText = balance.innerText - expensesOrSaving;
+        }
     }
     else {
-        alert("Please Provide valid information.")
+        alert("Please Provide valid information.");
     }
-
 }
+
+// income and expenses calculating
 document.getElementById('calculate').addEventListener("click", function () {
-    // get all input value
-    const income = inputChacking('income-field', 'income');
+    // get all input value from function
     const food = inputChacking('foot-field', 'food');
     const rent = inputChacking('rent-field', 'remt');
     const clothes = inputChacking('clothes-field', 'clothes');
 
+    // getting Total Expenses & Balance from html
     const totalExpenses = document.getElementById('total-expenses');
     const balance = document.getElementById('balance');
 
+    // Getting Total Expenses
     const expenses = parseInt(food) + parseInt(rent) + parseInt(clothes);
 
-    calculatingBalances(expenses, income, totalExpenses, balance, 'expenses');
-})
+    // calling calculatingBalances function
+    calculatingBalances(expenses, totalExpenses, balance, 'expenses', false);
+});
 
+// saving and Remaining Balance calculating
 document.getElementById('saving').addEventListener('click', function () {
+    // get input value from function
+    const income = inputChacking('income-field', 'income');
     const savingPercent = inputChacking('percent-input', 'save');
+
+    // getting Saving Amount, Remaining Balance & Balance from html
     const totalSaving = document.getElementById('saving-amount');
     const remainingBalance = document.getElementById('remaining-balance');
     const balance = document.getElementById('balance');
 
-    const saving = balance.innerText / 100 * savingPercent;
+    // Getting Total saving value
+    const saving = Number((income / 100 * savingPercent).toFixed(2));
 
-    if (0 < saving) {
-        calculatingBalances(saving, balance.innerText, totalSaving, remainingBalance, 'savings');
+    // Getting remaining balance value
+    const remainingBalances = Number((balance.innerText - saving).toFixed(2));
+
+    // calling calculatingBalances function
+    if (0 <= remainingBalances) {
+        calculatingBalances(saving, totalSaving, remainingBalance, 'savings', true);
     }
-})
+    else if (0 > remainingBalances) {
+        alert('Please Provide valid information.');
+    }
+});
